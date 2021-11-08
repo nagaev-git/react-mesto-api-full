ec57e5e5f16622bd957cbcad19cad067a5674199
Aleksandr Nagaev <nagaevvvvv@yandex.ru>
2021-11-08 14:27:51 +0300

    feat: test

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { celebrate, Joi, errors } = require("celebrate");
 const helmet = require("helmet");
+const cors = require("cors");
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -22,6 +23,7 @@ const allowedCors = [
   "http://localhost:3000",
 ];
 
+app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
